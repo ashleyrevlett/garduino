7a642bf2b54d8d01a5942865fa033e1f6c203351
ashley revlett <ashleyrevlett@gmail.com>
2013-02-27 01:54:40 -0500

    changed graphs from bar to line

--- server/public_html/main.js
+++ server/public_html/main.js
@@ -1,110 +1,75 @@
-		
-d3.text("data.csv", function(unparsedData) {
 
-	// read the csv file, cut it down to most recent 100 rows
-	var dataset = d3.csv.parseRows(unparsedData);
-	var totalReadings = 100;			
-	dataset = dataset.splice( dataset.length - totalReadings, totalReadings );
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+ 
 
-	// must multiply by 1000 to convert c++ epoch to js epoch
-	dataset.forEach(function(d) {
-		d[0] = d[0]*1000; 
+d3.csv("data.csv", function(error, data) {
+
+//	console.log(data);
+//	console.log(error);	
+
+	data.forEach(function(d) {
+		d.date = new Date( d.date * 1000 );
 	});
+ 
+	createBarChart(data, "temp");
+	createBarChart(data, "soil");
+	createBarChart(data, "light");    
 
-	createBarChart(dataset, 1, "#temp");
-	createBarChart(dataset, 3, "#light");
-	createBarChart(dataset, 4, "#soil_moisture");
-	createBarChart(dataset, 2, "#humidity");
-				
-}); // end .text
+});
+ 
+ 
+function createBarChart( data, csvColName, divID ) {
 
-		
-function createBarChart( dataset, csvColumnNumber, divID ) {
-	
-	// prepare the chart graphics
-	var barPadding = 0;	
-	var w = 760;
-	var h = 400;
-	var pad = 50;
+	var minDate = d3.min(data, function(d) { return d.date; });
+	var maxDate = d3.max(data, function(d) { return d.date; });
+	var minY = d3.min(data, function(d) { return d[ csvColName ]; });
+	var maxY = d3.max(data, function(d) { return d[ csvColName ]; });
 
-	// set up ranges and scales
-	var minDate = d3.min(dataset, function(d) { return new Date(d[0]); });
-	var maxDate = d3.max(dataset, function(d) { return new Date(d[0]); });
-	var minTemp = d3.min(dataset, function(d) { return d[ csvColumnNumber ]; });
-	var maxTemp = d3.max(dataset, function(d) { return d[ csvColumnNumber ]; });
-	var xScale = d3.time.scale().domain([minDate, maxDate]).range([0,w]);
-	var yScale = d3.scale.linear().domain([minTemp, maxTemp]).range([h,0]);
+	var x = d3.time.scale()
+		.domain([ minDate, maxDate ])
+		.range([ 0, width ]);	
+	var y = d3.scale.linear()
+		.domain([ minY, maxY ])
+		.range([height, 0]);
 
-	// draw the graph
-	var svg = d3.select( divID )
-	    .append("svg:svg")
-	    .attr("height", h + pad)
-	    .attr("width",  w + pad)
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+ 
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left");
 
-	// draw lines container
-	var vis = svg.append("svg:g")
-	    .attr("transform", "translate(40,20)")
-	var rules = vis.append("svg:g").classed("rules", true)
+	var line = d3.svg.line()
+	    .x(function(d) { return x( d.date ); })
+	    .y(function(d) { return y( d[csvColName] ); });
+ 
+	var svg = d3.select("#"+csvColName).append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+ 
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+ 
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text( csvColName );
+ 
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
 
-	// draw x and y axis
-	function make_x_axis() {
-	  return d3.svg.axis()
-	      .scale(xScale)
-	      .orient("bottom")
-	      .ticks(8)
-	}
-	function make_y_axis() {
-	  return d3.svg.axis()
-	      .scale(yScale)
-	      .orient("left")
-	      .ticks(10)
-	}
-	
-	// draw grid
-	rules.append("svg:g").classed("grid x_grid", true)
-	    .attr("transform", "translate(0,"+h+")")
-	    .call(make_x_axis()
-	      .tickSize(-h,0,0)
-	      .tickFormat("")
-	    )
-	rules.append("svg:g").classed("grid y_grid", true)
-	    .call(make_y_axis()
-	      .tickSize(-w,0,0)
-	      .tickFormat("")
-	    )
-	rules.append("svg:g").classed("labels x_labels", true)
-	    .attr("transform", "translate(0,"+h+")")
-	    .call(make_x_axis()
-	      .tickSize(10)
-	      .tickFormat(d3.time.format("%I:%M %p"))
-	    )
-	rules.append("svg:g").classed("labels y_labels", true)
-	    .call(make_y_axis()
-	      .tickSubdivide(1)
-	      .tickSize(10, 5, 0)
-	    )
-		
-	// draw bars
-	svg.append("svg:g").classed("bar_chart container", true)
-		.attr("transform", "translate(40,0)")
-		.selectAll("rect")
-		.data(dataset)
-		.enter()
-		.append("rect")
-		.attr("fill", "#cccccc")
-		.style("opacity", 0.9)
-		.attr("x", function(d,i) {
-			return xScale(d[0]);
-		})
-		.attr("y", function(d,i) {
-			return ( h - yScale(d[ csvColumnNumber ]) + 20 );
-		})
-		.attr("width", function(d,i) {
-			var barWidth = (w - (pad*2)) / dataset.length;
-			return (barWidth);
-		})
-		.attr("height", function(d,i) {
-			return yScale(d[ csvColumnNumber ]);						
-		});
-			
 }
